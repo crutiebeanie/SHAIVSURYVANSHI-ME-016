195b6838e877d4a8789cc56b8971e22b3f818b85
crutiebeanie <shaivsingh.suryavanshi@stu.adamasuniversity.ac.in>
2026-04-02 13:04:35 +0530

    Update gcdforloop.c

--- gcdforloop.c
+++ gcdforloop.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
-int gcdmain(int a, int b){
-    while(b!=0){
-        int temp=b;
-        b=a%b;
-        a=temp;
+
+int gcd(int a, int b) {
+    for (int temp; b != 0; ) {
+        temp = b;
+        b = a % b;
+        a = temp;
     }
     return a;
 }
+
 int main() {
-    int sug, diz;
-    scanf("%d",&sug);
-    scanf("%d",&diz);
-    int result=gcdmain(sug, diz);
-    printf("your result is %d", result);
+    int num1, num2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2)
+
+    printf("The GCD is %d\n", gcd(num1, num2));
+
     return 0;
 }
